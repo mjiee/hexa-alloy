348dd91732f4817aab6cf07d62e0722fcba076c1
mjiee <1083294650@qq.com>
2025-05-21 15:37:39 +0800

    feat: complete application navigation bar

---
+++ src/pages/home/HomePage.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="container mx-auto px-4 pt-4 pb-20 md:pb-4 md:pl-24">HomePage</div>
+</template>
--- src/pages/profile/ProfilePage.vue
+++ src/pages/profile/ProfilePage.vue
@@ -1,3 +1,3 @@
 <template>
-  <RouterView />
+  <div>Profile</div>
 </template>
